9170a5d05c49635cbbedf9bf0db7be3841fa20f9
unknown <brian.burgos@hotmail.com>
2018-04-26 16:51:39 -0300

    Se hace la vista para crear carpetas

--- TaskManager/TaskManager/Models/Carpeta.cs
+++ TaskManager/TaskManager/Models/Carpeta.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,17 @@ namespace TaskManager.Models
 {
     public class Carpeta
     {
-        public int IdCarpeta { get; set;}
+        public int IdCarpeta { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [RegularExpression(@"^([^<>]){1,50}$", ErrorMessage = "El campo {0} es obligatorio y tiene un máximo 50 caracteres.")]
+        [Display(Name = "Nombre *")]
         public string Nombre { get; set; }
+
+        [RegularExpression(@"^([^<>]){1,200}$", ErrorMessage = "El campo {0} tiene de máximo 200 caracteres.")]
+        [Display(Name = "Descripción")]
         public string Descripcion { get; set; }
+
         public DateTime FechaCreacion { get; set; }
         // public Usuario IdUsuario { get; set; }
     }
