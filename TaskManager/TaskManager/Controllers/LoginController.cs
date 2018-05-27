c4583cd7862f4eace3a1e0b3d5ebb557dec902d9
Maxipadula <padula.maximiliano@gmail.com>
2018-05-27 17:00:18 -0300

    Se agrega Parte de login -interaccion con base de datos por ahora comentada y no anda validacion por data anotaations

---
+++ TaskManager/TaskManager/Controllers/LoginController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TaskManager.Models;
+using TaskManager.Repository;
+
+namespace TaskManager.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ValidarLogin(Usuario user)
+        {
+            bool validacion = new LoginRepository().VerificarLogin(user);
+
+            if (validacion)
+            {
+                return RedirectToAction("Index", "Home", null);
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+   
+        public ActionResult Registracion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registracion(Usuario user)
+        {
+            if(new LoginRepository().RegistrarNewUser(user))
+            {
+              return RedirectToAction("ValidarLogin", user);
+            }
+            else
+            {
+                // Aca iria el return de la vista "amigable" de que ya existe un usuario activo con ese mail
+            }
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ TaskManager/TaskManager/Repository/LoginRepository.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TaskManager.Models;
+using System.Web.SessionState;
+
+namespace TaskManager.Repository
+{
+    public class LoginRepository : System.Web.UI.Page //Si no hereda de esta clase no me deja usar la variable de sesion
+    {
+        private static string mailTest = "Admin.admin@admin.com";
+        private static string passTest = "1234";
+
+        public bool VerificarLogin(Usuario usuario)
+        {
+            //Deberia verificar si el mail-contraseña que se intenta loguear coincide con alguno de la tabla
+            foreach (Usuario userInBase in GetUsers())
+            {
+                if (usuario.Email.Equals(userInBase.Email) && usuario.Contraseña.Equals(userInBase.Contraseña))
+                {
+                    /*Si encontramos coincidencia retornamos TRUE para que acceda al sistema y ya lo guardamos en la sesion 
+                    para mas adelante poder cargar la lista de carpetas de ESE usuario*/
+                    Session["userLogged"] = userInBase;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public List<Usuario> GetUsers()
+        {
+            //Provisorio, aca deberia traer lista de usuarios de la base de datos 
+            List<Usuario> usuarios = new List<Usuario>();
+
+            usuarios.Add(new Usuario { Email = mailTest, Contraseña = passTest });
+            return usuarios;
+        }
+
+        public bool RegistrarNewUser(Usuario user)
+        {
+            foreach (Usuario userInBase in GetUsers())
+            {
+                if (user.Email.Equals(userInBase.Email))
+                {
+                    if (user.Activo.Equals(1))
+                    {
+                        return false;
+                    }
+                    else if (user.Activo.Equals(0))
+                    {
+                        ActivarUsuario(user);
+                        //Agregar metodo para cambiar nombre, apellido y contraseña de usuario desactivado
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            ActivarUsuario(user);
+            //Agregar nuevo usuario a la tabla "Usuario" de la base -- Añadir en el insert del DAO la fecha de registro
+
+            return true;
+        }
+
+        public void ActivarUsuario(Usuario user)
+        {
+            DateTime LocalDate = DateTime.Now;
+            string Nombre = "General";
+            string Descripcion = "Carpeta por Defecto";
+
+            user.Activo = 1;
+        
+            if(user.Carpetas != null)
+            {
+                if(user.Carpetas.Count != 0)
+                {
+                    foreach (Carpeta carpeta in user.Carpetas)
+                    {
+                        if (carpeta.Nombre.Equals(Nombre))
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
+           
+            user.Carpetas.Add(new Carpeta {Nombre = Nombre, Descripcion = Descripcion, FechaCreacion = LocalDate });
+        }
+
+    }
+}
