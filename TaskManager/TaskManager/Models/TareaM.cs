4e81785e36d80102a4cd12a53bf88d089159c862
Facundo Migliorini <facundo.mig@gmail.com>
2018-06-19 21:12:23 -0300

    fix types

--- TaskManager/TaskManager/Models/TareaM.cs
+++ TaskManager/TaskManager/Models/TareaM.cs
@@ -1,48 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Web;
 
-namespace TaskManager.Models
-{
-    public class Tarea
+    namespace TaskManager.Models
     {
+        public class TareaM
+        {
 
-        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        [RegularExpression(@"^([^<>]){1,50}$", ErrorMessage = "El campo {0} es obligatorio y tiene de maximo 50 caracters.")]
-        [Display(Name = "Nombre")]
-        public string Nombre { get; set; }
+            [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+            [RegularExpression(@"^([^<>]){1,50}$", ErrorMessage = "El campo {0} es obligatorio y tiene de maximo 50 caracters.")]
+            [Display(Name = "Nombre")]
+            public string Nombre { get; set; }
 
-        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        [RegularExpression(@"^([^<>]){1,200}$", ErrorMessage = "El campo {0} es obligatorio y tiene de maximo 50 caracters.")]
-        [Display(Name = "Descripcion")]
-        public string Descripcion { get; set; }
+            [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+            [RegularExpression(@"^([^<>]){1,200}$", ErrorMessage = "El campo {0} es obligatorio y tiene de maximo 50 caracters.")]
+            [Display(Name = "Descripcion")]
+            public string Descripcion { get; set; }
 
-        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        [Display(Name = "Fecha Finalizacion")]
-        public DateTime FechaFin { get; set; }
+            [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+            [Display(Name = "Fecha Finalizacion")]
+            public DateTime? FechaFin { get; set; }
 
-        [Display(Name = "Fecha Creacion")]
-        public DateTime FechaCreacion { get; set; }
+            [Display(Name = "Fecha Creacion")]
+            public DateTime FechaCreacion { get; set; }
 
-        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        [Display(Name = "Prioridad")]
-        public int Prioridad { get; set; }
+            [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+            [Display(Name = "Prioridad")]
+            public short Prioridad { get; set; }
 
-        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
-        [Display(Name = "Carpeta")]
-        public string Carpeta { get; set; }
+            [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+            [Display(Name = "Carpeta")]
+            public string Carpeta { get; set; }
 
-        [Display(Name = "Estado")]
-        public int Completada { get; set; }
+            [Display(Name = "Estado")]
+            public short Completada { get; set; }
 
-        [Display(Name = "Horas Estimadas")]
-        public decimal EstimadoHoras { get; set; }
+            [Display(Name = "Horas Estimadas")]
+            public decimal? EstimadoHoras { get; set; }
 
-        public int IdTarea { get; set; }
-        //    IdCarpeta
-        //    IdUsuario
+            public int IdTarea { get; set; }
+            //    IdCarpeta
+            //    IdUsuario
 
+        }
     }
-}
